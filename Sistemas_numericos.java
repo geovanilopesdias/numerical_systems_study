02782144091e7e4bff931e558112b2a34314ed8b
Geovani Lopes Dias <geovani.phy.dias@gmail.com>
2023-03-10 17:56:01 -0300

    First day
    Conversion from binary, octal and hexadecimal OK.
    It still lacks a proper input text for hexadecimal values.

---
+++ Sistemas_numericos.java
@@ -0,0 +1,14 @@
+package com.gheowinapps.sistemas_numericos;
+import static com.gheowinapps.sistemas_numericos.ConversionModule.openConversionFromNonDecimalToDecimalScreen;
+/**
+ * Realizado como estudo dos sistemas de base binária, octal e hexadecimal.
+ * Compreende operações de conversões entre estes sistemas e operações aritméticas
+ * com base binária.
+ * @author Geovani Lopes Dias
+ */
+public class Sistemas_numericos {
+
+    public static void main(String[] args) {
+        openConversionFromNonDecimalToDecimalScreen();
+    }
+}
