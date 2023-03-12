5803661e5310bd7b1bd5d10d042f548b2a5f3eae
Geovani Lopes Dias <geovani.phy.dias@gmail.com>
2023-03-11 22:08:33 -0300

    Update BaseConversion.java

--- BaseConversion.java
+++ BaseConversion.java
@@ -5,44 +5,53 @@ package com.gheowinapps.sistemas_numericos;
  * @author Geovani Lopes Dias
  */
 public class BaseConversion {
+    
+    // Numerical tests:
     private static boolean isPurelyNumerical(String value){
-        boolean result = value.matches("\\d+");
-        if(result){return true;}
-        else{throw new RuntimeException("Input isn't purely numerical.");}
+        return value.matches("\\d+");
     }
     
-    private static boolean analyzeBinaryOrOctalInput(String value, int userBase){
+    
+    private static boolean isDecimalInputInByteRange(String textualValue) throws RuntimeException{
+        testNumericalInput(textualValue);
+        int value = Integer.parseInt(textualValue);
+        return value >= 0 && value <= 127;
+    }
+    
+    
+    private static void testNumericalInput(String value){
+        if(!isPurelyNumerical(value)){throw new RuntimeException("The input value isn't numerical.");}
+    }
+    
+    private static void testByteRange(String userValue, int neededBase){
+        if(neededBase == 2 && !isDecimalInputInByteRange(userValue)){
+            throw new RuntimeException("Apenas valores entre 0 e 127 são suportados bara conversão.");
+        }
+    }
+    
+    private static boolean analyzeBinaryOrOctalInput(String textualValue, int userBase){
+        testNumericalInput(textualValue);
         boolean userInputBaseIsSupported = (userBase == 2 || userBase == 8 || userBase == 16);
-        boolean userValueIsInteger = (int) Double.parseDouble(value) == Integer.parseInt(value);
+        boolean userValueIsInteger = (int) Double.parseDouble(textualValue) == Integer.parseInt(textualValue);
         
         if(!userInputBaseIsSupported) {throw new RuntimeException("Apenas base 2, 8 e 16 são suportadas.");}
         if(!userValueIsInteger) {throw new RuntimeException("Apenas valores inteiros são permitidos.");}
         return userValueIsInteger && userInputBaseIsSupported;
     }
     
-    private static boolean isDecimalInputInByteRange(String textualValue) throws RuntimeException{
-        if(isPurelyNumerical(textualValue)){
-            int value = Integer.parseInt(textualValue);
-            return value >= 0 && value <= 127;
-        }
-        return false;
-    }
-    
-    private static int countDigits(String value){
-        Double numericalValue = Double.valueOf(value);
+    // 
+    private static int countDigits(String textualValue){
+        testNumericalInput(textualValue);
+        Double numericalValue = Double.valueOf(textualValue);
         return (int) Math.log10(numericalValue)+1;
     }
     
     
-    private static int[] toIntArray(String value){
-        if(!isPurelyNumerical(value)){
-            throw new RuntimeException("Input isn't purely numerical.");
-        }
-        
-        int[] digits = new int[countDigits(value)];
-        for(int i = 0; i < countDigits(value); i++){
-            digits[i] = Character.getNumericValue(value.toCharArray()[i]);
-        }
+    private static int[] toIntArray(String textualValue){
+        testNumericalInput(textualValue);
+        int[] digits = new int[countDigits(textualValue)];
+        for(int i = 0; i < countDigits(textualValue); i++)
+            digits[i] = Character.getNumericValue(textualValue.toCharArray()[i]);
         return digits;
     }
     
@@ -78,10 +87,7 @@ public class BaseConversion {
             }
         }
     }
-    
-    
-    
-    
+
     
     private static boolean isDigitHexadecimalLetter(char letter){
         for (char hexLetter = 'A'; hexLetter <= 'F'; hexLetter++)
@@ -93,11 +99,11 @@ public class BaseConversion {
     
     
     // ---------- Specific conversion methods:
-    private static int convertBinaryOrOctalToDecimal(String value, int base) throws RuntimeException{
+    private static int convertBinaryOrOctalToDecimal(String userValue, int base) throws RuntimeException{
         int convertedValue = 0;
-        boolean inputIsOK = analyzeBinaryOrOctalInput(value, base);
+        boolean inputIsOK = analyzeBinaryOrOctalInput(userValue, base);
         if(inputIsOK){
-            int[] digitosDoValor = toIntArray(value);
+            int[] digitosDoValor = toIntArray(userValue);
             for (int i = 0; i < digitosDoValor.length; i++){
                 convertedValue += (int) digitosDoValor[i] * Math.pow(base, digitosDoValor.length-(1+i));
             }
@@ -108,27 +114,19 @@ public class BaseConversion {
     
     
     private static String convertDecimalToBinaryOrOctal(String userValue, int neededBase)throws RuntimeException{
-        if(!isPurelyNumerical(userValue)){
-            throw new RuntimeException("O valor inserido não é puramente numérico.");
-        }
+        testNumericalInput(userValue);
+        testByteRange(userValue, neededBase);
         
-        if(neededBase == 2 && !isDecimalInputInByteRange(userValue)){
-            throw new RuntimeException("Apenas valores entre 0 e 127 são suportados bara conversão.");
-        }
-        
-        else {
-            int currentQuotient = Integer.parseInt(userValue);
-            StringBuilder valueConvertedButInverted = new StringBuilder(); 
-            do{
-                valueConvertedButInverted.append(currentQuotient % neededBase);
-                currentQuotient /= neededBase;
-            } while (currentQuotient >= neededBase);
-            valueConvertedButInverted.append(currentQuotient % neededBase); // Último valor
-            return valueConvertedButInverted
-                    .reverse()
-                    .toString();
-        }
-        
+        int currentQuotient = Integer.parseInt(userValue);
+        StringBuilder valueConvertedButInverted = new StringBuilder(); 
+        do{
+            valueConvertedButInverted.append(currentQuotient % neededBase);
+            currentQuotient /= neededBase;
+        } while (currentQuotient >= neededBase);
+        valueConvertedButInverted.append(currentQuotient % neededBase); // Last value
+        return valueConvertedButInverted
+                .reverse()
+                .toString();
     }
     
     
@@ -146,30 +144,24 @@ public class BaseConversion {
     }
     
     
-    
     private static String convertDecimalToHexadecimal(String userValue) throws RuntimeException{
-        if(!isPurelyNumerical(userValue)){
-            throw new RuntimeException("O valor inserido não é puramente numérico.");
-        }
+        testNumericalInput(userValue);
         
-        else {
-            int currentQuotient = Integer.parseInt(userValue), currentRemainder;
-            StringBuilder valueConvertedButInverted = new StringBuilder();
-            char nextDigit;
-            do{
-                currentRemainder = currentQuotient % 16;
-                nextDigit = convertNumberToHexadecimalLetter(currentRemainder);
-                valueConvertedButInverted.append(nextDigit);
-                currentQuotient /= 16;
-            } while (currentQuotient >= 16);
-            
-            // Último dígito:
+        int currentQuotient = Integer.parseInt(userValue), currentRemainder;
+        StringBuilder valueConvertedButInverted = new StringBuilder();
+        char nextDigit;
+        do{
             currentRemainder = currentQuotient % 16;
             nextDigit = convertNumberToHexadecimalLetter(currentRemainder);
             valueConvertedButInverted.append(nextDigit);
+            currentQuotient /= 16;
+        } while (currentQuotient >= 16);
 
-            return valueConvertedButInverted.reverse().toString();
-        }
+        // Last digit insertion:
+        currentRemainder = currentQuotient % 16;
+        nextDigit = convertNumberToHexadecimalLetter(currentRemainder);
+        valueConvertedButInverted.append(nextDigit);
+        return valueConvertedButInverted.reverse().toString();
     }
     
     // ----------- General conversion functions:
@@ -201,7 +193,5 @@ public class BaseConversion {
         return null;
     }
     
-    
-    /* ----- Screen-I/O functions -----*/
     
 }
